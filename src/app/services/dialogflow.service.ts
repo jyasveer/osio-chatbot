5f25314888eeebfa100785b687743b72391a367f
Jyasveer Gotta <jyasveer@gmail.com>
2018-07-30 15:38:13 +0530

    feat(initial-commit): first commit

--- src/app/services/dialogflow.service.ts
+++ src/app/services/dialogflow.service.ts
@@ -1,32 +1,31 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
-import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
-import { environment } from '@env/environment';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class DialogflowService {
 
-  private baseURL: string = "https://api.dialogflow.com/v1/query?v=20150910";
+  private baseURL = 'https://api.dialogflow.com/v1/query?v=20150910';
   private token: string = environment.token;
 
-  constructor(private http: Http){}
+  constructor(private http: Http) { }
 
-  public getResponse(query: string){
-    let data = {
-      query : query,
+  public getResponse(query: string) {
+    const data = {
+      query: query,
       lang: 'en',
       sessionId: '12345'
     }
     return this.http
-      .post(`${this.baseURL}`, data, {headers: this.getHeaders()})
+      .post(`${this.baseURL}`, data, { headers: this.getHeaders() })
       .map(res => {
-        return res.json()
-      })
+        return res.json();
+      });
   }
 
-  public getHeaders(){
-    let headers = new Headers();
+  public getHeaders() {
+    const headers = new Headers();
     headers.append('Authorization', `Bearer ${this.token}`);
     return headers;
   }
